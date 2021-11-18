48bd897aa9b3f5db2d4ad27047aaa71be884186b
Wagner Gonçalves <wagner@ar.com.br>
2021-11-18 20:05:21 -0300

    Commit inicial

---
+++ src/main/java/br/com/fiap/microservicos/PS2/Ps2Application.java
@@ -0,0 +1,13 @@
+package br.com.fiap.microservicos.PS2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ps2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ps2Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/microservicos/PS2/Ps2ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.microservicos.PS2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ps2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
